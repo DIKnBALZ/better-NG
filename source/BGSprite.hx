84b25656422255a961d1e15b9a3aa878595c3e56
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2022-09-05 20:08:19 -0500

    more things that i did
    yahoo

--- source/BGSprite.hx
+++ source/BGSprite.hx
@@ -2,27 +2,19 @@ package;
 
 import flixel.FlxSprite;
 
-class BGSprite extends FlxSprite
-{
+class BGSprite extends FlxSprite {
 	public var idleAnim:String = null;
-
-	override public function new(image:String, x:Float = 0, y:Float = 0, scrollX:Float = 1, scrollY:Float = 1, animations:Array<String> = null, loopAnims:Bool = false)
-	{
+	override public function new(image:String, x:Float = 0, y:Float = 0, scrollX:Float = 1, scrollY:Float = 1, animations:Array<String> = null, loopAnims:Bool = false) {
 		super(x, y);
-		
-		if (animations != null)
-		{
+		if (animations != null) {
 			frames = Paths.getSparrowAtlas(image);
-			for (anim in animations)
-			{
+			for (anim in animations) {
 				animation.addByPrefix(anim, anim, 24, loopAnims);
 				animation.play(anim);
-				if (idleAnim == null)
-					idleAnim = anim;
+				if (idleAnim == null) idleAnim = anim;
 			}
 		}
-		else
-		{
+		else {
 			loadGraphic(Paths.image(image));
 			active = false;
 		}
@@ -30,9 +22,7 @@ class BGSprite extends FlxSprite
 		antialiasing = true;
 	}
 
-	public function dance()
-	{
-		if (idleAnim != null)
-			animation.play(idleAnim);
+	public function dance() {
+		if (idleAnim != null) animation.play(idleAnim);
 	}
 }
